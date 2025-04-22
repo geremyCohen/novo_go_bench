cccabe9b7136e5341277a794b4dc2ca219f4e56d
Geremy Cohen <geremy.cohen@arm.com>
2025-04-22 16:04:50 -0700

    more debug

--- run_go_benchmarks.sh
+++ run_go_benchmarks.sh
@@ -1,5 +1,7 @@
 out=/tmp/results.csv
 cd /novo_go_bench
+
 export GOPATH="$(dirname "$(readlink -f "$0")")"
+go mod download golang.org/x/crypto >> $out 2>&1
 echo "Go performance" >> $out 2>&1
 go run ./go_benchmarks.go >> $out 2>&1
