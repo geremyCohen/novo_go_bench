71538d4978c46723c6def3cc0a8a3065a91782f2
Geremy Cohen <geremy.cohen@arm.com>
2025-04-22 15:34:45 -0700

    Cleaning up uneeded code

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -8,24 +8,19 @@ fi
 
 echo $out
 
-nprocs=`cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l`
-
 # Install script for Ubuntu 24.04 LTS
 sudo apt update
 sudo rm -rf /usr/local/go
 sudo rm -rf /usr/bin/go
 sudo snap install go --classic
 sudo ln -s /snap/bin/go /usr/bin/go
-git clone https://github.com/cloudflare/cf_benchmark
-cd cf_benchmark
+git clone https://github.com/geremyCohen/novo_go_bench.git
+cd novo_go_bench
 
 go mod download golang.org/x/crypto
 go install
 
 export GOPATH="$(dirname "$(readlink -f "$0")")"
-
-#go get -u -v golang.org/x/crypto/chacha20poly1305/...
-
 
 echo "Go performance" | tee -a $out
 go run ./go_benchmarks.go | tee -a $out
